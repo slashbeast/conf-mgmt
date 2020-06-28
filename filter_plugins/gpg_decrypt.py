39a73a35b6ff814be0aa281f9b8122669dadf3d0
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2020-06-28 21:58:44 +0200

    filter_plugins/gpg_decrypt.py: be more strict.
    It's bytes-like object, but could be StrictUndefined, so str() +
    encode() to make sure it's the way it should be.

--- filter_plugins/gpg_decrypt.py
+++ filter_plugins/gpg_decrypt.py
@@ -5,7 +5,7 @@ gpg = gnupg.GPG()
 
 
 def gpg_decrypt(string):
-    return gpg.decrypt(string).data.decode('utf-8')
+    return gpg.decrypt(str(string).encode('utf-8')).data.decode('utf-8')
 
 
 class FilterModule(object):

540391c6fc35b261d7c913846c6401c854a0a386
KARBOWSKI Piotr <piotr.karbowski@gmail.com>
2020-09-04 17:13:46 +0200

    filter_plugins/gpg_decrypt.py: optimize.

--- filter_plugins/gpg_decrypt.py
+++ filter_plugins/gpg_decrypt.py
@@ -1,10 +1,14 @@
-import gnupg
+from functools import lru_cache
 
 
-gpg = gnupg.GPG()
+@lru_cache
+def get_gpg():
+    import gnupg
+    return gnupg.GPG()
 
 
 def gpg_decrypt(string):
+    gpg = get_gpg()
     return gpg.decrypt(str(string).encode('utf-8')).data.decode('utf-8')
 
 
